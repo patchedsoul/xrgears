8fcc1b7dad3799ebf827f501acde01dcfdd52b32
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-07-06 17:06:43 +0200

    move offscreen pass code to new class.

--- examples/hmddistortion/VikOffscreenPass.hpp
+++ examples/hmddistortion/VikOffscreenPass.hpp
@@ -1,10 +1,246 @@
+#include <vulkan/vulkan.h>
+
+#include "VulkanDevice.hpp"
+
+// Texture properties
+#define TEX_DIM 2048
+#define TEX_FILTER VK_FILTER_LINEAR
+
+// Offscreen frame buffer properties
+#define FB_DIM TEX_DIM
 
 class VikOffscreenPass {
 public:
-    VikOffscreenPass() {
+
+    VkDevice device;
+
+    // One sampler for the frame buffer color attachments
+    VkSampler colorSampler;
+
+    // Framebuffer for offscreen rendering
+    struct FrameBufferAttachment {
+	VkImage image;
+	VkDeviceMemory mem;
+	VkImageView view;
+	VkFormat format;
+    };
+    struct FrameBuffer {
+	int32_t width, height;
+	VkFramebuffer frameBuffer;
+	FrameBufferAttachment position;
+	FrameBufferAttachment depth;
+	VkRenderPass renderPass;
+    } offScreenFrameBuf;
+
+    VikOffscreenPass(VkDevice& d) {
+	device = d;
+    }
+
+    ~VikOffscreenPass() {
+	 vkDestroySampler(device, colorSampler, nullptr);
+
+	// Color attachments
+	vkDestroyImageView(device, offScreenFrameBuf.position.view, nullptr);
+	vkDestroyImage(device, offScreenFrameBuf.position.image, nullptr);
+	vkFreeMemory(device, offScreenFrameBuf.position.mem, nullptr);
+
+	// Depth attachment
+	vkDestroyImageView(device, offScreenFrameBuf.depth.view, nullptr);
+	vkDestroyImage(device, offScreenFrameBuf.depth.image, nullptr);
+	vkFreeMemory(device, offScreenFrameBuf.depth.mem, nullptr);
+
+	vkDestroyFramebuffer(device, offScreenFrameBuf.frameBuffer, nullptr);
+
+	vkDestroyRenderPass(device, offScreenFrameBuf.renderPass, nullptr);
 
     }
-    ~VikOffscreenPass() {
 
+    // Create a frame buffer attachment
+    void createAttachment(
+            vks::VulkanDevice *vulkanDevice,
+            VkFormat format,
+            VkImageUsageFlagBits usage,
+            FrameBufferAttachment *attachment)
+    {
+	VkImageAspectFlags aspectMask = 0;
+	VkImageLayout imageLayout;
+
+	attachment->format = format;
+
+	if (usage & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)
+	{
+	    aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	    imageLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	}
+	if (usage & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
+	{
+	    aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+	    imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+	}
+
+	assert(aspectMask > 0);
+
+	VkImageCreateInfo image = vks::initializers::imageCreateInfo();
+	image.imageType = VK_IMAGE_TYPE_2D;
+	image.format = format;
+	image.extent.width = offScreenFrameBuf.width;
+	image.extent.height = offScreenFrameBuf.height;
+	image.extent.depth = 1;
+	image.mipLevels = 1;
+	image.arrayLayers = 1;
+	image.samples = VK_SAMPLE_COUNT_1_BIT;
+	image.tiling = VK_IMAGE_TILING_OPTIMAL;
+	image.usage = usage | VK_IMAGE_USAGE_SAMPLED_BIT;
+
+	VkMemoryAllocateInfo memAlloc = vks::initializers::memoryAllocateInfo();
+	VkMemoryRequirements memReqs;
+
+	VK_CHECK_RESULT(vkCreateImage(device, &image, nullptr, &attachment->image));
+	vkGetImageMemoryRequirements(device, attachment->image, &memReqs);
+	memAlloc.allocationSize = memReqs.size;
+	memAlloc.memoryTypeIndex = vulkanDevice->getMemoryType(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &attachment->mem));
+	VK_CHECK_RESULT(vkBindImageMemory(device, attachment->image, attachment->mem, 0));
+
+	VkImageViewCreateInfo imageView = vks::initializers::imageViewCreateInfo();
+	imageView.viewType = VK_IMAGE_VIEW_TYPE_2D;
+	imageView.format = format;
+	imageView.subresourceRange = {};
+	imageView.subresourceRange.aspectMask = aspectMask;
+	imageView.subresourceRange.baseMipLevel = 0;
+	imageView.subresourceRange.levelCount = 1;
+	imageView.subresourceRange.baseArrayLayer = 0;
+	imageView.subresourceRange.layerCount = 1;
+	imageView.image = attachment->image;
+	VK_CHECK_RESULT(vkCreateImageView(device, &imageView, nullptr, &attachment->view));
+    }
+
+    // Prepare a new framebuffer and attachments for offscreen rendering (G-Buffer)
+    void prepareOffscreenFramebuffer(vks::VulkanDevice *vulkanDevice, VkPhysicalDevice& physicalDevice)
+    {
+	offScreenFrameBuf.width = FB_DIM;
+	offScreenFrameBuf.height = FB_DIM;
+
+	// Color attachments
+
+	// (World space) Positions
+	createAttachment(
+	            vulkanDevice,
+	            VK_FORMAT_R16G16B16A16_SFLOAT,
+	            VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+	            &offScreenFrameBuf.position);
+
+	// Depth attachment
+
+	// Find a suitable depth format
+	VkFormat attDepthFormat;
+	VkBool32 validDepthFormat = vks::tools::getSupportedDepthFormat(physicalDevice, &attDepthFormat);
+	assert(validDepthFormat);
+
+	createAttachment(
+	            vulkanDevice,
+	            attDepthFormat,
+	            VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+	            &offScreenFrameBuf.depth);
+
+	// Set up separate renderpass with references to the color and depth attachments
+	std::array<VkAttachmentDescription, 2> attachmentDescs = {};
+
+	// Init attachment properties
+	for (uint32_t i = 0; i < 2; ++i)
+	{
+	    attachmentDescs[i].samples = VK_SAMPLE_COUNT_1_BIT;
+	    attachmentDescs[i].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	    attachmentDescs[i].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	    attachmentDescs[i].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	    attachmentDescs[i].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	    if (i == 1)
+	    {
+		attachmentDescs[i].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		attachmentDescs[i].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+	    }
+	    else
+	    {
+		attachmentDescs[i].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		attachmentDescs[i].finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+	    }
+	}
+
+	// Formats
+	attachmentDescs[0].format = offScreenFrameBuf.position.format;
+	attachmentDescs[1].format = offScreenFrameBuf.depth.format;
+
+	std::vector<VkAttachmentReference> colorReferences;
+	colorReferences.push_back({ 0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL });
+
+	VkAttachmentReference depthReference = {};
+	depthReference.attachment = 1;
+	depthReference.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+	VkSubpassDescription subpass = {};
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.pColorAttachments = colorReferences.data();
+	subpass.colorAttachmentCount = static_cast<uint32_t>(colorReferences.size());
+	subpass.pDepthStencilAttachment = &depthReference;
+
+	// Use subpass dependencies for attachment layput transitions
+	std::array<VkSubpassDependency, 2> dependencies;
+
+	dependencies[0].srcSubpass = VK_SUBPASS_EXTERNAL;
+	dependencies[0].dstSubpass = 0;
+	dependencies[0].srcStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
+	dependencies[0].dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependencies[0].srcAccessMask = VK_ACCESS_MEMORY_READ_BIT;
+	dependencies[0].dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	dependencies[0].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+
+	dependencies[1].srcSubpass = 0;
+	dependencies[1].dstSubpass = VK_SUBPASS_EXTERNAL;
+	dependencies[1].srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependencies[1].dstStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
+	dependencies[1].srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	dependencies[1].dstAccessMask = VK_ACCESS_MEMORY_READ_BIT;
+	dependencies[1].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+
+	VkRenderPassCreateInfo renderPassInfo = {};
+	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	renderPassInfo.pAttachments = attachmentDescs.data();
+	renderPassInfo.attachmentCount = static_cast<uint32_t>(attachmentDescs.size());
+	renderPassInfo.subpassCount = 1;
+	renderPassInfo.pSubpasses = &subpass;
+	renderPassInfo.dependencyCount = 2;
+	renderPassInfo.pDependencies = dependencies.data();
+
+	VK_CHECK_RESULT(vkCreateRenderPass(device, &renderPassInfo, nullptr, &offScreenFrameBuf.renderPass));
+
+	std::array<VkImageView,2> attachments;
+	attachments[0] = offScreenFrameBuf.position.view;
+	attachments[1] = offScreenFrameBuf.depth.view;
+
+	VkFramebufferCreateInfo fbufCreateInfo = {};
+	fbufCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	fbufCreateInfo.pNext = NULL;
+	fbufCreateInfo.renderPass = offScreenFrameBuf.renderPass;
+	fbufCreateInfo.pAttachments = attachments.data();
+	fbufCreateInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
+	fbufCreateInfo.width = offScreenFrameBuf.width;
+	fbufCreateInfo.height = offScreenFrameBuf.height;
+	fbufCreateInfo.layers = 1;
+	VK_CHECK_RESULT(vkCreateFramebuffer(device, &fbufCreateInfo, nullptr, &offScreenFrameBuf.frameBuffer));
+
+	// Create sampler to sample from the color attachments
+	VkSamplerCreateInfo sampler = vks::initializers::samplerCreateInfo();
+	sampler.magFilter = VK_FILTER_NEAREST;
+	sampler.minFilter = VK_FILTER_NEAREST;
+	sampler.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	sampler.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+	sampler.addressModeV = sampler.addressModeU;
+	sampler.addressModeW = sampler.addressModeU;
+	sampler.mipLodBias = 0.0f;
+	sampler.maxAnisotropy = 0;
+	sampler.minLod = 0.0f;
+	sampler.maxLod = 1.0f;
+	sampler.borderColor = VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE;
+	VK_CHECK_RESULT(vkCreateSampler(device, &sampler, nullptr, &colorSampler));
     }
 };
