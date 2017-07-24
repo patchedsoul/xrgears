1885e554f5022ee09d44db7e7e8e60d33c8035d7
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-07-24 15:32:19 +0200

    camera: move stereo cam code to class.

--- examples/xrgears/xrgears.cpp
+++ examples/xrgears/xrgears.cpp
@@ -34,6 +34,7 @@
 #include "VikCamera.hpp"
 #include "VikHMD.hpp"
 #include "VikBuffer.hpp"
+#include "VikCameraStereo.hpp"
 
 #define VERTEX_BUFFER_BIND_ID 0
 #define ENABLE_VALIDATION true
@@ -51,6 +52,8 @@ public:
   VikNode* teapotNode;
 
   VikHMD* hmd;
+
+  VikCameraStereo* vikCamera;
 
   bool enableSky = true;
 
@@ -74,11 +77,8 @@ public:
     glm::vec4 lights[4];
   } uboLights;
 
-  //UBOCamera uboCamera;
-
   struct {
     vks::Buffer lights;
-    //vks::Buffer camera;
   } uniformBuffers;
 
   struct {
@@ -92,13 +92,6 @@ public:
   VkCommandBuffer offScreenCmdBuffer = VK_NULL_HANDLE;
   // Semaphore used to synchronize between offscreen and final scene rendering
   VkSemaphore offscreenSemaphore = VK_NULL_HANDLE;
-
-  // Camera and view properties
-  float eyeSeparation = 0.08f;
-  const float focalLength = 0.5f;
-  const float fov = 90.0f;
-  const float zNear = 0.1f;
-  const float zFar = 256.0f;
 
   VulkanExample() : VulkanExampleBase(ENABLE_VALIDATION)
   {
@@ -617,59 +610,6 @@ public:
     updateLights();
   }
 
-/*
-  void updateCamera() {
-    // Geometry shader matrices for the two viewports
-    // See http://paulbourke.net/stereographics/stereorender/
-
-    // Calculate some variables
-    float aspectRatio = (float)(width * 0.5f) / (float)height;
-    float wd2 = zNear * tan(glm::radians(fov / 2.0f));
-    float ndfl = zNear / focalLength;
-    float left, right;
-    float top = wd2;
-    float bottom = -wd2;
-
-    glm::vec3 camFront;
-    camFront.x = -cos(glm::radians(rotation.x)) * sin(glm::radians(rotation.y));
-    camFront.y = sin(glm::radians(rotation.x));
-    camFront.z = cos(glm::radians(rotation.x)) * cos(glm::radians(rotation.y));
-    camFront = glm::normalize(camFront);
-    glm::vec3 camRight = glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f)));
-
-    glm::mat4 rotM = glm::mat4();
-    glm::mat4 transM;
-
-    rotM = glm::rotate(rotM, glm::radians(camera.rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
-    rotM = glm::rotate(rotM, glm::radians(camera.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-    rotM = glm::rotate(rotM, glm::radians(camera.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
-
-    // Left eye
-    left = -aspectRatio * wd2 + 0.5f * eyeSeparation * ndfl;
-    right = aspectRatio * wd2 + 0.5f * eyeSeparation * ndfl;
-
-    transM = glm::translate(glm::mat4(), camera.position - camRight * (eyeSeparation / 2.0f));
-
-    uboCamera.projection[0] = glm::frustum(left, right, bottom, top, zNear, zFar);
-    uboCamera.view[0] = rotM * transM;
-    uboCamera.skyView[0] = rotM * glm::translate(glm::mat4(), -camRight * (eyeSeparation / 2.0f));
-
-    // Right eye
-    left = -aspectRatio * wd2 - 0.5f * eyeSeparation * ndfl;
-    right = aspectRatio * wd2 - 0.5f * eyeSeparation * ndfl;
-
-    transM = glm::translate(glm::mat4(), camera.position + camRight * (eyeSeparation / 2.0f));
-
-    uboCamera.projection[1] = glm::frustum(left, right, bottom, top, zNear, zFar);
-    uboCamera.view[1] = rotM * transM;
-    uboCamera.skyView[1] = rotM * glm::translate(glm::mat4(), camRight * (eyeSeparation / 2.0f));;
-
-    uboCamera.position = camera.position * -1.0f;
-
-    memcpy(uniformBuffers.camera.mapped, &uboCamera, sizeof(uboCamera));
-  }
-  */
-
   void updateLights()
   {
     const float p = 15.0f;
@@ -799,7 +739,7 @@ public:
 
   void changeEyeSeparation(float delta)
   {
-    eyeSeparation += delta;
+    vikCamera->changeEyeSeparation(delta);
     updateUniformBuffers();
   }
 
