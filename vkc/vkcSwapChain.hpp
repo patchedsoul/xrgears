6bddc7cc92f82218053ca619bbfb9978015be490
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-09-08 23:11:03 +0200

    indent.

--- vkc/vkcSwapChain.hpp
+++ vkc/vkcSwapChain.hpp
@@ -7,48 +7,47 @@
 namespace vkc {
 
 struct RenderBuffer {
-    VkImage image;
-    VkImageView view;
-    VkFramebuffer framebuffer;
+  VkImage image;
+  VkImageView view;
+  VkFramebuffer framebuffer;
 };
 
 class SwapChain {
 public:
+  RenderBuffer buffers[MAX_NUM_IMAGES];
 
-    RenderBuffer buffers[MAX_NUM_IMAGES];
+  SwapChain() {}
+  ~SwapChain() {}
 
-    SwapChain() {}
-    ~SwapChain() {}
-
-    void init_buffer(VkDevice device, VkFormat image_format, VkRenderPass render_pass,
-                     uint32_t width, uint32_t height, RenderBuffer *b) {
-  VkImageViewCreateInfo imageviewinfo = {
+  void init_buffer(VkDevice device, VkFormat image_format, VkRenderPass render_pass,
+                   uint32_t width, uint32_t height, RenderBuffer *b) {
+    VkImageViewCreateInfo imageviewinfo = {
       .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
       .image = b->image,
       .viewType = VK_IMAGE_VIEW_TYPE_2D,
       .format = image_format,
       .components = {
-          .r = VK_COMPONENT_SWIZZLE_R,
-          .g = VK_COMPONENT_SWIZZLE_G,
-          .b = VK_COMPONENT_SWIZZLE_B,
-          .a = VK_COMPONENT_SWIZZLE_A,
+        .r = VK_COMPONENT_SWIZZLE_R,
+        .g = VK_COMPONENT_SWIZZLE_G,
+        .b = VK_COMPONENT_SWIZZLE_B,
+        .a = VK_COMPONENT_SWIZZLE_A,
       },
       .subresourceRange = {
-          .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-          .baseMipLevel = 0,
-          .levelCount = 1,
-          .baseArrayLayer = 0,
-          .layerCount = 1,
+        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+        .baseMipLevel = 0,
+        .levelCount = 1,
+        .baseArrayLayer = 0,
+        .layerCount = 1,
       },
-  };
+    };
 
-  vkCreateImageView(device,
-                    &imageviewinfo,
-                    NULL,
-                    &b->view);
+    vkCreateImageView(device,
+                      &imageviewinfo,
+                      NULL,
+                      &b->view);
 
 
-  VkFramebufferCreateInfo framebufferinfo = {
+    VkFramebufferCreateInfo framebufferinfo = {
       .sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
       .renderPass = render_pass,
       .attachmentCount = 1,
@@ -56,14 +55,12 @@ public:
       .width = width,
       .height = height,
       .layers = 1
-  };
+    };
 
-  vkCreateFramebuffer(device,
-                      &framebufferinfo,
-                      NULL,
-                      &b->framebuffer);
-    }
-
-
+    vkCreateFramebuffer(device,
+                        &framebufferinfo,
+                        NULL,
+                        &b->framebuffer);
+  }
 };
 }
