ab58a7dc7a7823f13cbb6bc7972b8cc95dd72727
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-09-25 21:47:39 +0200

    vks: move tools to header.

--- vks/vksTools.hpp
+++ vks/vksTools.hpp
@@ -48,33 +48,184 @@ namespace vks {
 namespace tools {
 
 /** @brief Returns the device type as a string */
-std::string physicalDeviceTypeString(VkPhysicalDeviceType type);
+static std::string physicalDeviceTypeString(VkPhysicalDeviceType type) {
+  switch (type) {
+#define STR(r) case VK_PHYSICAL_DEVICE_TYPE_ ##r: return #r
+    STR(OTHER);
+    STR(INTEGRATED_GPU);
+    STR(DISCRETE_GPU);
+    STR(VIRTUAL_GPU);
+#undef STR
+    default: return "UNKNOWN_DEVICE_TYPE";
+  }
+}
 
 // Selected a suitable supported depth format starting with 32 bit down to 16 bit
 // Returns false if none of the depth formats in the list is supported by the device
-VkBool32 getSupportedDepthFormat(VkPhysicalDevice physicalDevice, VkFormat *depthFormat);
+static VkBool32 getSupportedDepthFormat(VkPhysicalDevice physicalDevice, VkFormat *depthFormat) {
+  // Since all depth formats may be optional, we need to find a suitable depth format to use
+  // Start with the highest precision packed format
+  std::vector<VkFormat> depthFormats = {
+    VK_FORMAT_D32_SFLOAT_S8_UINT,
+    VK_FORMAT_D32_SFLOAT,
+    VK_FORMAT_D24_UNORM_S8_UINT,
+    VK_FORMAT_D16_UNORM_S8_UINT,
+    VK_FORMAT_D16_UNORM
+  };
+
+  for (auto& format : depthFormats) {
+    VkFormatProperties formatProps;
+    vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &formatProps);
+    // Format must support depth stencil attachment for optimal tiling
+    if (formatProps.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
+      *depthFormat = format;
+      return true;
+    }
+  }
+  return false;
+}
 
 // Put an image memory barrier for setting an image layout on the sub resource into the given command buffer
-void setImageLayout(
+// Create an image memory barrier for changing the layout of
+// an image and put it into an active command buffer
+// See chapter 11.4 "Image Layout" for details
+
+static void setImageLayout(
     VkCommandBuffer cmdbuffer,
     VkImage image,
     VkImageLayout oldImageLayout,
     VkImageLayout newImageLayout,
     VkImageSubresourceRange subresourceRange,
     VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
-    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT) {
+  // Create an image barrier object
+  VkImageMemoryBarrier imageMemoryBarrier = vks::initializers::imageMemoryBarrier();
+  imageMemoryBarrier.oldLayout = oldImageLayout;
+  imageMemoryBarrier.newLayout = newImageLayout;
+  imageMemoryBarrier.image = image;
+  imageMemoryBarrier.subresourceRange = subresourceRange;
+
+  // Source layouts (old)
+  // Source access mask controls actions that have to be finished on the old layout
+  // before it will be transitioned to the new layout
+  switch (oldImageLayout) {
+    case VK_IMAGE_LAYOUT_UNDEFINED:
+      // Image layout is undefined (or does not matter)
+      // Only valid as initial layout
+      // No flags required, listed only for completeness
+      imageMemoryBarrier.srcAccessMask = 0;
+      break;
+
+    case VK_IMAGE_LAYOUT_PREINITIALIZED:
+      // Image is preinitialized
+      // Only valid as initial layout for linear images, preserves memory contents
+      // Make sure host writes have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+      // Image is a color attachment
+      // Make sure any writes to the color buffer have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+      // Image is a depth/stencil attachment
+      // Make sure any writes to the depth/stencil buffer have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
+      // Image is a transfer source
+      // Make sure any reads from the image have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+      // Image is a transfer destination
+      // Make sure any writes to the image have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+      // Image is read by a shader
+      // Make sure any shader reads from the image have been finished
+      imageMemoryBarrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+      break;
+    default:
+      // Other source layouts aren't handled (yet)
+      break;
+  }
+
+  // Target layouts (new)
+  // Destination access mask controls the dependency for the new image layout
+  switch (newImageLayout) {
+    case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+      // Image will be used as a transfer destination
+      // Make sure any writes to the image have been finished
+      imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
+      // Image will be used as a transfer source
+      // Make sure any reads from the image have been finished
+      imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+      // Image will be used as a color attachment
+      // Make sure any writes to the color buffer have been finished
+      imageMemoryBarrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+      // Image layout will be used as a depth/stencil attachment
+      // Make sure any writes to depth/stencil buffer have been finished
+      imageMemoryBarrier.dstAccessMask = imageMemoryBarrier.dstAccessMask | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+      break;
+
+    case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+      // Image will be read in a shader (sampler, input attachment)
+      // Make sure any writes to the image have been finished
+      if (imageMemoryBarrier.srcAccessMask == 0)
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT | VK_ACCESS_TRANSFER_WRITE_BIT;
+      imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+      break;
+    default:
+      // Other source layouts aren't handled (yet)
+      break;
+  }
+
+  // Put barrier inside setup command buffer
+  vkCmdPipelineBarrier(
+        cmdbuffer,
+        srcStageMask,
+        dstStageMask,
+        0,
+        0, nullptr,
+        0, nullptr,
+        1, &imageMemoryBarrier);
+}
 // Uses a fixed sub resource layout with first mip level and layer
-void setImageLayout(
+// Fixed sub resource on first mip level and layer
+static void setImageLayout(
     VkCommandBuffer cmdbuffer,
     VkImage image,
     VkImageAspectFlags aspectMask,
     VkImageLayout oldImageLayout,
     VkImageLayout newImageLayout,
     VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
-    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT) {
+  VkImageSubresourceRange subresourceRange = {};
+  subresourceRange.aspectMask = aspectMask;
+  subresourceRange.baseMipLevel = 0;
+  subresourceRange.levelCount = 1;
+  subresourceRange.layerCount = 1;
+  setImageLayout(cmdbuffer, image, oldImageLayout, newImageLayout, subresourceRange, srcStageMask, dstStageMask);
+}
 
 /** @brief Inser an image memory barrier into the command buffer */
-void insertImageMemoryBarrier(
+static void insertImageMemoryBarrier(
     VkCommandBuffer cmdbuffer,
     VkImage image,
     VkAccessFlags srcAccessMask,
@@ -83,19 +234,62 @@ void insertImageMemoryBarrier(
     VkImageLayout newImageLayout,
     VkPipelineStageFlags srcStageMask,
     VkPipelineStageFlags dstStageMask,
-    VkImageSubresourceRange subresourceRange);
+    VkImageSubresourceRange subresourceRange) {
+  VkImageMemoryBarrier imageMemoryBarrier = vks::initializers::imageMemoryBarrier();
+  imageMemoryBarrier.srcAccessMask = srcAccessMask;
+  imageMemoryBarrier.dstAccessMask = dstAccessMask;
+  imageMemoryBarrier.oldLayout = oldImageLayout;
+  imageMemoryBarrier.newLayout = newImageLayout;
+  imageMemoryBarrier.image = image;
+  imageMemoryBarrier.subresourceRange = subresourceRange;
+
+  vkCmdPipelineBarrier(
+        cmdbuffer,
+        srcStageMask,
+        dstStageMask,
+        0,
+        0, nullptr,
+        0, nullptr,
+        1, &imageMemoryBarrier);
+}
 
 // Display error message and exit on fatal error
 //void exitFatal(std::string message, std::string caption);
 
 // Load a SPIR-V shader (binary)
-VkShaderModule loadShader(const char *fileName, VkDevice device);
+static VkShaderModule loadShader(const char *fileName, VkDevice device) {
+  std::ifstream is(fileName, std::ios::binary | std::ios::in | std::ios::ate);
 
-// Load a GLSL shader (text)
-// Note: GLSL support requires vendor-specific extensions to be enabled and is not a core-feature of Vulkan
-VkShaderModule loadShaderGLSL(const char *fileName, VkDevice device, VkShaderStageFlagBits stage);
+  if (is.is_open()) {
+    size_t size = is.tellg();
+    is.seekg(0, std::ios::beg);
+    char* shaderCode = new char[size];
+    is.read(shaderCode, size);
+    is.close();
+
+    assert(size > 0);
+
+    VkShaderModule shaderModule;
+    VkShaderModuleCreateInfo moduleCreateInfo{};
+    moduleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    moduleCreateInfo.codeSize = size;
+    moduleCreateInfo.pCode = (uint32_t*)shaderCode;
+
+    vik_log_check(vkCreateShaderModule(device, &moduleCreateInfo, NULL, &shaderModule));
+
+    delete[] shaderCode;
+
+    return shaderModule;
+  } else {
+    std::cerr << "Error: Could not open shader file \"" << fileName << "\"" << std::endl;
+    return VK_NULL_HANDLE;
+  }
+}
 
 /** @brief Checks if a file exists */
-bool fileExists(const std::string &filename);
+static bool fileExists(const std::string &filename) {
+  std::ifstream f(filename.c_str());
+  return !f.fail();
+}
 }  // namespace tools
 }  // namespace vks
