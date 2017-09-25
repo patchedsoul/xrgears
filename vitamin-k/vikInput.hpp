b7595e74d4dec45fb5a4f6f59b214f7d901584a3
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-09-25 18:11:43 +0200

    add vikinput class.

---
+++ vitamin-k/vikInput.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace vik {
+class Input {
+public:
+    enum MouseButton {
+	Left,
+	Middle,
+	Right
+    };
+
+    enum MouseScrollAxis {
+	X,
+	Y
+    };
+
+    enum Key {
+	ESCAPE,
+	SPACE,
+	F1,
+	W,
+	A,
+	S,
+	D,
+	P
+    };
+
+};
+}
