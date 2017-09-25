6f694a2d7b7e44d8c79569857dce4f5a1d0e7e82
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2017-09-25 20:13:25 +0200

    wayland: sort functions.

--- vitamin-k/vikWindowWaylandShell.hpp
+++ vitamin-k/vikWindowWaylandShell.hpp
@@ -11,10 +11,8 @@
 
 #pragma once
 
-
 #include <vulkan/vulkan.h>
 
-#include <string>
 #include "vikWindowWayland.hpp"
 #include "../vks/vksSwapChain.hpp"
 
@@ -26,8 +24,7 @@ class WindowWaylandShell : public WindowWayland {
   wl_pointer *pointer = nullptr;
   wl_shell_surface *shell_surface = nullptr;
 
-
- public:
+public:
   explicit WindowWaylandShell() {
     name = "wayland-shell";
     display = wl_display_connect(NULL);
@@ -37,7 +34,7 @@ class WindowWaylandShell : public WindowWayland {
     vik_log_f_if(!registry, "Could not get Wayland registry!");
 
     static const struct wl_registry_listener registry_listener =
-      { registryGlobalCb, registryGlobalRemoveCb };
+    { registryGlobalCb, registryGlobalRemoveCb };
     wl_registry_add_listener(registry, &registry_listener, this);
     wl_display_dispatch(display);
     wl_display_roundtrip(display);
@@ -58,16 +55,33 @@ class WindowWaylandShell : public WindowWayland {
     wl_display_disconnect(display);
   }
 
+  int init(vik::Renderer *r) {
+    surface = wl_compositor_create_surface(compositor);
+    shell_surface = wl_shell_get_shell_surface(shell, surface);
+
+    static const struct wl_shell_surface_listener shell_surface_listener =
+    { PingCb, ConfigureCb, PopupDoneCb };
+
+    wl_shell_surface_add_listener(shell_surface, &shell_surface_listener, this);
+    //wl_shell_surface_set_toplevel(shell_surface);
+
+    vik_log_d("setting hmd refresh to %d", hmd_refresh);
+    vik_log_d("setting hmd output to %p", hmd_output);
+
+    if (r->settings->fullscreen)
+      wl_shell_surface_set_fullscreen(shell_surface,
+                                      WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
+                                      hmd_refresh,
+                                      hmd_output);
+    return 0;
+  }
+
   void iterate(vik::Renderer *r) {
     while (wl_display_prepare_read(display) != 0)
       wl_display_dispatch_pending(display);
     wl_display_flush(display);
     wl_display_read_events(display);
     wl_display_dispatch_pending(display);
-  }
-
-  void update_window_title(const std::string& title) {
-    wl_shell_surface_set_title(shell_surface, title.c_str());
   }
 
   void init_swap_chain(vik::Renderer *r) {
@@ -81,105 +95,24 @@ class WindowWaylandShell : public WindowWayland {
     sc->select_queue_and_format();
   }
 
-  static void registryGlobalCb(void *data, wl_registry *registry, uint32_t name,
-                               const char *interface, uint32_t version) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    self->registryGlobal(registry, name, interface, version);
+  void update_window_title(const std::string& title) {
+    wl_shell_surface_set_title(shell_surface, title.c_str());
   }
 
+  static void outputModeCb(void *data, struct wl_output *wl_output,
+                           unsigned int flags, int w, int h, int refresh) {
+    vik_log_i("outputModeCb: %dx%d@%d", w, h, refresh);
 
-  static void outputGeometryCb(void *data, struct wl_output *wl_output, int x,
-      int y, int w, int h, int subpixel, const char *make, const char *model,
-      int transform) {
-    //VikWindowWayland *self = reinterpret_cast<VikWindowWayland *>(data);
-        vik_log_i("%s: %s [%d, %d] %dx%d", make, model, x, y, w, h);
-  }
-
-    static void outputModeCb(void *data, struct wl_output *wl_output, 
-          unsigned int flags, int w, int h, int refresh) {
-        vik_log_i("outputModeCb: %dx%d@%d", w, h, refresh);
-        
     //    if (w == 2560 && h == 1440) {
-        if (w == 1920 && h == 1200) {
-          WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-          vik_log_d("setting wl_output to %p", wl_output);
-          self->hmd_output = wl_output;
-          self->hmd_refresh = refresh;
-        } else {
-          vik_log_d("ignoring wl_output %p", wl_output);
-        }
+    if (w == 1920 && h == 1200) {
+      WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+      vik_log_d("setting wl_output to %p", wl_output);
+      self->hmd_output = wl_output;
+      self->hmd_refresh = refresh;
+    } else {
+      vik_log_d("ignoring wl_output %p", wl_output);
     }
-
-    static void
-    outputDoneCb(void *data, struct wl_output *output) {
-        vik_log_d("output done %p", output);
-    }
-
-    static void
-    outputScaleCb(void *data, struct wl_output *output, int scale) {
-        vik_log_d("output scale: %d", scale);
-    }
-
-  static void seatCapabilitiesCb(void *data, wl_seat *seat, uint32_t caps) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    self->seatCapabilities(seat, caps);
   }
-
-  static void pointerEnterCb(void *data, wl_pointer *pointer, uint32_t serial,
-                             wl_surface *surface, wl_fixed_t sx, wl_fixed_t sy)
-  {}
-
-  static void pointerLeaveCb(void *data, wl_pointer *pointer, uint32_t serial,
-                             wl_surface *surface)
-  {}
-
-  static void pointerMotionCb(void *data, wl_pointer *pointer, uint32_t time,
-                              wl_fixed_t x, wl_fixed_t y) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    self->pointer_motion_cb(wl_fixed_to_double(x),
-                            wl_fixed_to_double(y));
-  }
-
-  static void pointerButtonCb(void *data,
-                              wl_pointer *pointer, uint32_t serial, uint32_t time, uint32_t button,
-                              uint32_t state) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    self->pointer_button_cb(wayland_to_vik_button(button), state);
-  }
-
-  static void pointerAxisCb(void *data,
-                            wl_pointer *pointer, uint32_t time, uint32_t axis,
-                            wl_fixed_t value) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    double d = wl_fixed_to_double(value);
-    self->pointer_axis_cb(wayland_to_vik_axis(axis), d);
-  }
-
-  static void keyboardKeymapCb(void *data,
-                               struct wl_keyboard *keyboard, uint32_t format, int fd, uint32_t size)
-  {}
-
-  static void keyboardEnterCb(void *data,
-                              struct wl_keyboard *keyboard, uint32_t serial,
-                              struct wl_surface *surface, struct wl_array *keys)
-  {}
-
-  static void keyboardLeaveCb(void *data,
-                              struct wl_keyboard *keyboard, uint32_t serial,
-                              struct wl_surface *surface)
-  {}
-
-  static void keyboardKeyCb(void *data,
-                            struct wl_keyboard *keyboard, uint32_t serial, uint32_t time,
-                            uint32_t key, uint32_t state) {
-    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
-    self->keyboard_key_cb(wayland_to_vik_key(key), state);
-  }
-
-  static void keyboardModifiersCb(void *data, struct wl_keyboard *keyboard,
-                                  uint32_t serial, uint32_t mods_depressed,
-                                  uint32_t mods_latched, uint32_t mods_locked,
-                                  uint32_t group) {}
 
   void seatCapabilities(wl_seat *seat, uint32_t caps) {
     if ((caps & WL_SEAT_CAPABILITY_POINTER) && !pointer) {
@@ -220,10 +153,10 @@ class WindowWaylandShell : public WindowWayland {
       wl_output* the_output = (wl_output*) wl_registry_bind(registry, name, &wl_output_interface, 2);
 
       static const struct wl_output_listener _output_listener = {
-         outputGeometryCb,
-         outputModeCb,
-         outputDoneCb,
-         outputScaleCb
+        outputGeometryCb,
+            outputModeCb,
+            outputDoneCb,
+            outputScaleCb
       };
 
       wl_output_add_listener(the_output, &_output_listener, this);
@@ -231,40 +164,109 @@ class WindowWaylandShell : public WindowWayland {
     }
   }
 
-  static void registryGlobalRemoveCb(void *data, struct wl_registry *registry,
-                                     uint32_t name) {}
+  // debug callbacks
+  static void ConfigureCb(void *data, struct wl_shell_surface *shell_surface,
+                          uint32_t edges, int32_t width, int32_t height) {
+    vik_log_d("configure: %dx%d", width, height);
+  }
+
+  static void
+  outputDoneCb(void *data, struct wl_output *output) {
+    vik_log_d("output done %p", output);
+  }
+
+  static void
+  outputScaleCb(void *data, struct wl_output *output, int scale) {
+    vik_log_d("output scale: %d", scale);
+  }
+
+  static void outputGeometryCb(void *data, struct wl_output *wl_output, int x,
+                               int y, int w, int h, int subpixel, const char *make, const char *model,
+                               int transform) {
+    //VikWindowWayland *self = reinterpret_cast<VikWindowWayland *>(data);
+    vik_log_i("%s: %s [%d, %d] %dx%d", make, model, x, y, w, h);
+  }
+
+  // callback wrappers
 
   static void PingCb(void *data, struct wl_shell_surface *shell_surface,
                      uint32_t serial) {
     wl_shell_surface_pong(shell_surface, serial);
   }
 
-  static void ConfigureCb(void *data, struct wl_shell_surface *shell_surface,
-                          uint32_t edges, int32_t width, int32_t height) {
-    vik_log_d("configure: %dx%d", width, height);
+  static void registryGlobalCb(void *data, wl_registry *registry, uint32_t name,
+                               const char *interface, uint32_t version) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    self->registryGlobal(registry, name, interface, version);
   }
+
+  static void keyboardKeyCb(void *data,
+                            struct wl_keyboard *keyboard, uint32_t serial, uint32_t time,
+                            uint32_t key, uint32_t state) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    self->keyboard_key_cb(wayland_to_vik_key(key), state);
+  }
+
+  static void seatCapabilitiesCb(void *data, wl_seat *seat, uint32_t caps) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    self->seatCapabilities(seat, caps);
+  }
+
+  static void pointerMotionCb(void *data, wl_pointer *pointer, uint32_t time,
+                              wl_fixed_t x, wl_fixed_t y) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    self->pointer_motion_cb(wl_fixed_to_double(x),
+                            wl_fixed_to_double(y));
+  }
+
+  static void pointerButtonCb(void *data,
+                              wl_pointer *pointer, uint32_t serial, uint32_t time, uint32_t button,
+                              uint32_t state) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    self->pointer_button_cb(wayland_to_vik_button(button), state);
+  }
+
+  static void pointerAxisCb(void *data,
+                            wl_pointer *pointer, uint32_t time, uint32_t axis,
+                            wl_fixed_t value) {
+    WindowWaylandShell *self = reinterpret_cast<WindowWaylandShell *>(data);
+    double d = wl_fixed_to_double(value);
+    self->pointer_axis_cb(wayland_to_vik_axis(axis), d);
+  }
+
+  // Unused callbacks
+  static void registryGlobalRemoveCb(void *data, struct wl_registry *registry,
+                                     uint32_t name) {}
 
   static void PopupDoneCb(void *data, struct wl_shell_surface *shell_surface) {}
 
-  int init(vik::Renderer *r) {
-    surface = wl_compositor_create_surface(compositor);
-    shell_surface = wl_shell_get_shell_surface(shell, surface);
+  static void keyboardModifiersCb(void *data, struct wl_keyboard *keyboard,
+                                  uint32_t serial, uint32_t mods_depressed,
+                                  uint32_t mods_latched, uint32_t mods_locked,
+                                  uint32_t group) {}
 
-    static const struct wl_shell_surface_listener shell_surface_listener =
-    { PingCb, ConfigureCb, PopupDoneCb };
+  static void keyboardKeymapCb(void *data,
+                               struct wl_keyboard *keyboard, uint32_t format, int fd, uint32_t size)
+  {}
 
-    wl_shell_surface_add_listener(shell_surface, &shell_surface_listener, this);
-    //wl_shell_surface_set_toplevel(shell_surface);
-    
-    vik_log_d("setting hmd refresh to %d", hmd_refresh);
-    vik_log_d("setting hmd output to %p", hmd_output);
+  static void keyboardEnterCb(void *data,
+                              struct wl_keyboard *keyboard, uint32_t serial,
+                              struct wl_surface *surface, struct wl_array *keys)
+  {}
 
-    if (r->settings->fullscreen)
-      wl_shell_surface_set_fullscreen(shell_surface,
-                                      WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
-                                      hmd_refresh,
-                                      hmd_output);
-    return 0;
-  }
+  static void keyboardLeaveCb(void *data,
+                              struct wl_keyboard *keyboard, uint32_t serial,
+                              struct wl_surface *surface)
+  {}
+
+  static void pointerEnterCb(void *data, wl_pointer *pointer, uint32_t serial,
+                             wl_surface *surface, wl_fixed_t sx, wl_fixed_t sy)
+  {}
+
+  static void pointerLeaveCb(void *data, wl_pointer *pointer, uint32_t serial,
+                             wl_surface *surface)
+  {}
+
+
 };
 }
